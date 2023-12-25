3abd2d3c8f5521606ddd24971cce3f1d8e0039c5
rlawotmd <kimjs2400@naver.com>
2023-12-25 23:54:52 +0900

    solved

---
+++ src/BAEKJOON11382.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class BAEKJOON11382 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
---
+++ src/BAEKJOON2743.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class BAEKJOON2743 {
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        String s = br.readLine();
+        System.out.println(s.length());
+        //StringTokenizer st = new StringTokenizer(s);
+
+        }
+}
