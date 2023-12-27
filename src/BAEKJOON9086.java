9c792b9846b3d0a559f17e4d6505d595500443c0
rlawotmd <kimjs2400@naver.com>
2023-12-27 22:08:36 +0900

    solved

--- src/BAEKJOON9086.java
+++ src/BAEKJOON9086.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class BAEKJOON9086 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        /*
         for(int i = 0; i )
+
+         */
     }
 }
