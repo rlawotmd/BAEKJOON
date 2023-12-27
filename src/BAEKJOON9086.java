ca5983e7925d6180fd1e2813669971eaa9b91557
rlawotmd <kimjs2400@naver.com>
2023-12-27 23:30:24 +0900

    solved

--- src/BAEKJOON9086.java
+++ src/BAEKJOON9086.java
@@ -1,11 +1,25 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class BAEKJOON9086 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        /*
-        for(int i = 0; i )
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] words = new String[n];
+        String a = null;
+        String b = null;
+        for(int i = 0; i < n; i++)
+            words[i] = br.readLine();
 
-         */
+        for(int i = 0; i < n; i++) {
+            if(words[i].length() == 0) {
+                a = words[i]; b = words[i];
+            } else {
+                a = words[i].substring(0, 1);
+                b = words[i].substring(words[i].length() - 1);
+            }
+            System.out.println(a + b);
+        }
+
     }
 }
