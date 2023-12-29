dd5bf7196ac488d7a1045f45c6b86b0424c53105
rlawotmd <kimjs2400@naver.com>
2023-12-30 01:22:00 +0900

    solved

--- src/BAEKJOON2738.java
+++ src/BAEKJOON2738.java
@@ -1,5 +1,29 @@
-public class BAEKJOON2738 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class BAEKJOON2738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num_a = Integer.parseInt(st.nextToken());
+        int num_b = Integer.parseInt(st.nextToken());
+
+        int[][] arr_1 = new int[num_b][num_a];
+
+        for(int i = 0; i < num_a; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < num_b; j++) {
+                arr_1[j][i] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < num_a; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < num_b; j++) {
+                System.out.print(arr_1[j][i] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            System.out.println();
+        }
     }
 }
