89bff03978f3d946300d7fd4579aaed93cb02c7d
rlawotmd <kimjs2400@naver.com>
2024-01-02 21:37:24 +0900

    solved

---
+++ src/BAEKJOON27433.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BAEKJOON27433 {
+    public static void main(String[] args) {
+        BigInteger[] arr = new BigInteger[21];
+        BigInteger fec = BigInteger.valueOf(1);
+        arr[0] = BigInteger.valueOf(1);
+        arr[1] = BigInteger.valueOf(1);
+        for(int i = 2; i <= 20; i++) {
+            arr[i] = BigInteger.valueOf(i).multiply(arr[i - 1]);
+        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(arr[n]);
+
+    }
+}
