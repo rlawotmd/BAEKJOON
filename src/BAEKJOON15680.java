46ccf5fa83d7707d3611d3ef9e535acea80f42ed
rlawotmd <kimjs2400@naver.com>
2024-01-02 21:09:08 +0900

    solved

---
+++ src/BAEKJOON15680.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON15680 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == 0) System.out.println("YONSEI");
+        if(n == 1) System.out.println("Leading the Way to the Future");
+    }
+}
