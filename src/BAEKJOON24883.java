0e119105ce17355d36e0a09ea6d557656c1254e8
rlawotmd <kimjs2400@naver.com>
2024-01-05 15:52:25 +0900

    solved

---
+++ src/BAEKJOON24883.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON24883 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if(s.equalsIgnoreCase("n")) System.out.println("Naver D2");
+        else System.out.println("Naver Whale");
+    }
+}
