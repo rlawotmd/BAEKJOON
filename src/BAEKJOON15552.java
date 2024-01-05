9b3e90b6fb81cfe4ed9d07b120952d4876ce16b9
rlawotmd <kimjs2400@naver.com>
2024-01-05 19:05:30 +0900

    solved

---
+++ src/BAEKJOON15552.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BAEKJOON15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a + b));
+            bw.write("\n");
+        }
+        bw.flush();
+    }
+}
