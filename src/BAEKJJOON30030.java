740be5013dcc681445becf309ddb06256d08996a
rlawotmd <kimjs2400@naver.com>
2024-01-10 20:38:50 +0900

    solved

---
+++ src/BAEKJJOON30030.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BAEKJJOON30030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        System.out.println((b - (b /11 * 1)));
+    }
+}
