ee9824f4bf5d3434720c78a0931f4b421681a410
rlawotmd <kimjs2400@naver.com>
2024-01-16 00:53:14 +0900

    📝 solved

---
+++ src/BAEKJOON3046.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(((m * 2) - n));
+    }
+}
