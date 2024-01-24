779604f5833e0b5f7816f21d456280c4116f1b7e
rlawotmd <kimjs2400@naver.com>
2024-01-25 00:25:38 +0900

    📝 solved

---
+++ src/BAEKJOON1264.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BAEKJOON1264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        int a = 0;
+        while(true) {
+            String s = sc.nextLine();
+            if(s.trim().equals("#")) System.exit(0);
+            n = s.length();
+            s = s.replace("a", "");
+            s = s.replace("e", "");
+            s = s.replace("i", "");
+            s = s.replace("o", "");
+            s = s.replace("u", "");
+            s = s.replace("A", "");
+            s = s.replace("E", "");
+            s = s.replace("I", "");
+            s = s.replace("O", "");
+            s = s.replace("U", "");
+            a = s.length();
+            System.out.println((n - a));
+        }
+    }
+}
