ce934328b490c13597ee18abe11ea2f455021c1f
rlawotmd <kimjs2400@naver.com>
2024-01-30 00:14:01 +0900

    📝 solved

---
+++ src/BAEKJOON24263.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
