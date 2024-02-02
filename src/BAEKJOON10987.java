049327ec9bc64a3a4aa9b50ce202727a8b6ddede
rlawotmd <kimjs2400@naver.com>
2024-02-03 02:14:51 +0900

    📝 solved

--- src/BAEKJOON10987.java
+++ src/BAEKJOON10987.java
@@ -4,9 +4,9 @@ public class BAEKJOON10987 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        int n = s.length();
+        int n = 0;
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') n--;
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') n++;
         }
         System.out.println(n);
     }
