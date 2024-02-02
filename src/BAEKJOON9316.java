e262e47db2a1baa805401fedb71af42a7e670e4b
rlawotmd <kimjs2400@naver.com>
2024-02-02 23:07:39 +0900

    📝 solved

---
+++ src/BAEKJOON9316.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BAEKJOON9316 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            System.out.println("Hello World, Judge " + (n + 1) + "!");
+        }
+    }
+}
