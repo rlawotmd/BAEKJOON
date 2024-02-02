aacdf824051f9369e28cf8b6552ea5d1cc2aca63
rlawotmd <kimjs2400@naver.com>
2024-02-02 23:08:26 +0900

    📝 solved

--- src/BAEKJOON9316.java
+++ src/BAEKJOON9316.java
@@ -5,7 +5,7 @@ public class BAEKJOON9316 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++) {
-            System.out.println("Hello World, Judge " + (n + 1) + "!");
+            System.out.println("Hello World, Judge " + (i + 1) + "!");
         }
     }
 }
