fe51079cf07f0a65fbb1e3a759db3126d8d8d95e
rlawotmd <kimjs2400@naver.com>
2024-02-17 23:50:04 +0900

    📝 solved

---
+++ src/BAEKJOON16486.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(((a * 2) + ((b * 2) * 3.141592)));
+    }
+}
