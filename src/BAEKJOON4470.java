f7a39a3badb21f874835f27b3370524c00236717
rlawotmd <kimjs2400@naver.com>
2024-03-03 03:02:15 +0900

    📝 rename

--- src/BAEKJOON4470.java
+++ src/BAEKJOON4470.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class BAAEKJOON4470 {
+public class BAEKJOON4470 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
