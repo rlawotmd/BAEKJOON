fc101355e2c8ddbe246e9e53055e0b1dfc384582
rlawotmd <kimjs2400@naver.com>
2024-03-14 23:50:10 +0900

    📝 solved

---
+++ src/BAEKJOON11023.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON11023 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = st.countTokens();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+        System.out.println(sum);
+    }
+}
