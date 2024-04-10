2ca38e7ba658f87c2ce23b5fc0da37ef0bdec8ba
rlawotmd <kimjs2400@naver.com>
2024-04-10 15:41:58 +0900

    :memo: solved

--- src/BAEKJOON11651.java
+++ src/BAEKJOON11651.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class BAEKJOON11651 {
     public static void main(String[] args) throws IOException {
@@ -16,21 +10,17 @@ public class BAEKJOON11651 {
         int[][] arr = new int[n][2];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][0] = Integer.parseInt(st.nextToken());
-                arr[i][1] = Integer.parseInt(st.nextToken());
-            }
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr, (o1, o2) -> {
-            return o2[0]-o1[0];
+            return o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1];
         });
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.println(arr[i][0] + " " + arr[i][1]);
-            }
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         }
 
     }
