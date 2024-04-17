256228a7030345df6114f3acc5e006a0d4e36651
rlawotmd <kimjs2400@naver.com>
2024-04-17 23:50:28 +0900

    :memo: solved

---
+++ src/BAEKJOON10189.java
@@ -0,0 +1,8 @@
+public class BAEKJOON10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
