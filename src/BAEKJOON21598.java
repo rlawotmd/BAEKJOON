145ff9c278dec28a8eaf31470d28f4e6424f27e0
rlawotmd <kimjs2400@naver.com>
2024-05-26 22:30:50 +0900

    📝 solved

---
+++ src/BAEKJOON21598.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BAEKJOON21598 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for (long i = 0; i < n; i++) System.out.println("SciComLove");
+    }
+}
