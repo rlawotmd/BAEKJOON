9e4919d98645c859d5c120532ed64d3d2c6e4c15
rlawotmd <kimjs2400@naver.com>
2024-05-27 23:28:29 +0900

    :memo: solved

---
+++ src/BAEKJOON26766.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BAEKJOON26766 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(" @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+        }
+    }
+}
