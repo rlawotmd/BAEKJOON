ca1350c2d7f1bb04c80caf143b5a79c7d08ba1d5
rlawotmd <kimjs2400@naver.com>
2024-05-30 23:39:58 +0900

    📝 solved

---
+++ src/BAEKJOON29751.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON29751 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Double n = sc.nextDouble();
+        Double m = sc.nextDouble();
+        System.out.println((n * m) / 2);
+    }
+}
