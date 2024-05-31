1d5e338b69c65876a3a8756d3ec748661dbe3305
김재승 <117148033+rlawotmd@users.noreply.github.com>
2024-05-31 23:57:07 +0900

    Update BAEKJOON10039.java

--- src/BAEKJOON10039.java
+++ src/BAEKJOON10039.java
@@ -7,7 +7,7 @@ public class BAEKJOON10039 {
         for(int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
-        /*int n1 = sc.nextInt();
+        /*
         int n2 = sc.nextInt();
         int n3 = sc.nextInt();
         int n4 = sc.nextInt();
