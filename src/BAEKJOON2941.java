df7b36808740d94d5104171e14e9c6a2a9e04a54
rlawotmd <kimjs2400@naver.com>
2024-06-12 19:27:29 +0900

    :memo: solved

--- src/BAEKJOON2941.java
+++ src/BAEKJOON2941.java
@@ -1,48 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class BAEKJOON2941 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int leng = s.length();
         while (s.contains("=") || s.contains("-") || s.contains("lj") || s.contains("nj")) {
             if (s.contains("c=")) {
-                leng--;
                 s = s.replace("c=", "a");
             }
             if (s.contains("c-")) {
-                leng--;
                 s = s.replace("c-", "a");
             }
             if (s.contains("dz=")) {
-                leng--;
                 s = s.replace("dz=", "a");
             }
             if (s.contains("d-")) {
-                leng--;
                 s = s.replace("d-", "a");
             }
             if (s.contains("s=")) {
-                leng--;
                 s = s.replace("s=", "a");
             }
             if (s.contains("z=")) {
-                leng--;
                 s = s.replace("z=", "a");
             }
             if (s.contains("z=")) {
-                leng--;
                 s = s.replace("z=", "a");
             }
             if (s.contains("lj")) {
-                leng--;
                 s = s.replace("lj", "a");
             }
             if (s.contains("nj")) {
-                leng--;
                 s = s.replace("nj", "a");
             }
         }
