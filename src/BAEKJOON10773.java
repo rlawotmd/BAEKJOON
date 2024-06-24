5cd61904f1e44144440fb4f8e8eec0c5aee919ff
rlawotmd <kimjs2400@naver.com>
2024-06-24 13:43:23 +0900

    :memo: solved

--- src/BAEKJOON10773.java
+++ src/BAEKJOON10773.java
@@ -1,23 +1,25 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 
 public class BAEKJOON10773 {
-    public static Stack<Integer> stack = new Stack<>();
+    static ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int num;
-        for (int i = 0; i < n; i++) {
-            num = sc.nextInt();
-            if (num == 0) stack.pop();
-            else stack.push(num);
-        }
-
         int sum = 0;
-
-        for (int i = 0; i < stack.size(); i++) {
-            sum += stack.elementAt(i);
+        for (int i = 0; i < n; i++) {
+            num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sum -= stack.peek();
+                stack.pop();
+            } else {
+                stack.push(num);
+                sum += num;
+            }
         }
 
         System.out.println(sum);
--- src/BAEKJOON2164.java
+++ src/BAEKJOON2164.java
@@ -1,13 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class BAEKJOON2164 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        Queue<Integer> q = new LinkedList<Integer>();
+        ArrayDeque<Integer> q = new ArrayDeque<>();
 
         for (int i = 1; i <= n; i++) {
             q.add(i);
