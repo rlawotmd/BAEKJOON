1199fd1f971eb37a45afab421a8073d1b0cdb611
rlawotmd <kimjs2400@naver.com>
2024-06-25 15:50:43 +0900

    :memo: solved

--- src/BAEKJOON1181.java
+++ src/BAEKJOON1181.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class BAEKJOON1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         String[] arr = new String[n];
@@ -12,21 +13,28 @@ public class BAEKJOON1181 {
             arr[i] = br.readLine();
         }
 
-        String[] new_arr = Arrays.stream(arr).distinct().toArray(String[]::new);
-        Arrays.sort(new_arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                } else {
-                    return o1.length() - o2.length();
-                }
-            }
-        });
 
-        for (int i = 0; i < new_arr.length; i++) {
-            System.out.println(new_arr[i]);
+        String[] new_arr2 = Arrays.stream(arr).distinct().toArray(String[]::new);
+        Arrays.sort(new_arr2, (o1, o2) ->
+            o1.length() == o2.length() ? o1.compareTo(o2) : o1.length() - o2.length()
+        );
+//        String[] new_arr = Arrays.stream(arr).distinct().toArray(String[]::new);
+//        Arrays.sort(new_arr, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                if (o1.length() == o2.length()) {
+//                    return o1.compareTo(o2);
+//                } else {
+//                    return o1.length() - o2.length();
+//                }
+//            }
+//        });
+
+        for (int i = 0; i < new_arr2.length; i++) {
+            bw.write(new_arr2[i] + "\n");
         }
+
+        bw.flush();
 
     }
 }
