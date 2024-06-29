5cf6b380287227ce9110d68094be492fa7bccf20
rlawotmd <kimjs2400@naver.com>
2024-06-29 22:54:30 +0900

    :memo: solved

---
+++ src/BAEKJOON11719.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+
+    }
+}
