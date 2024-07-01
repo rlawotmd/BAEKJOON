981d0b79da3b27ebacd6b4655fc819a3e15fe263
rlawotmd <kimjs2400@naver.com>
2024-07-01 17:30:33 +0900

    :memo: solved (BFS)

---
+++ src/BAEKJOON1743.java
@@ -0,0 +1,77 @@
+// BFS
+
+import java.io.*;
+import java.util.*;
+
+public class BAEKJOON1743 {
+    static int[][] a;
+    static boolean[][] v;
+    static int n, m, k, max = 0;
+    static int[] di = {-1, 0, 1, 0};
+    static int[] dj = {0, 1, 0, -1};
+
+    static void BFS(int x, int y) {
+        int size = 0;
+        v[x][y] = true;
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{x, y});
+        while (!q.isEmpty()) {
+            size++;
+            int[] c = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int dx = c[0] + di[i];
+                int dy = c[1] + dj[i];
+                if (dx < 0 || dy < 0 || dx >= n || dy >= m || v[dx][dy] || a[dx][dy] != 1) continue;
+                v[dx][dy] = true;
+                q.offer(new int[]{dx, dy});
+            }
+        }
+        max = Math.max(max, size);
+    }
+
+    static void DFS(int x, int y) {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        a = new int[n][m];
+        v = new boolean[n][m];
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            a[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1] = 1;
+        }
+        for (int[] bbb : a) System.out.println(Arrays.toString(bbb));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (v[i][j] || a[i][j] != 1) continue;
+//                DFS(i, j);
+                BFS(i, j);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
+
+/*
+3 4 9
+1 1
+2 1
+3 1
+3 2
+3 3
+3 4
+2 4
+1 4
+1 3
+ans = 9
+ */
