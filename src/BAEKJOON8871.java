5ec6e1f0f79787922dbeae97a9bbb1cd1768bee1
rlawotmd <kimjs2400@naver.com>
2024-07-24 14:27:03 +0900

    :memo: solved

---
+++ src/BAEKJOON8871.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BAEKJOON8871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print((n + 1) * 2 + " " + (n + 1) * 3);
+    }
+}
