bef194de133f5a57c1062e4ef9d1bd34dc4ab7b5
rlawotmd <kimjs2400@naver.com>
2024-08-05 14:10:13 +0900

    :memo: solved

---
+++ src/BAEKJOON9295.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BAEKJOON9295 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case " + i + ": " + (a + b));
+        }
+    }
+}
