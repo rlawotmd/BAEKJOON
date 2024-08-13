a62ecc223c947bb1c1f39c89cf747c56b9e8a319
rlawotmd <kimjs2400@naver.com>
2024-08-13 20:30:10 +0900

    :memo: solved

---
+++ src/BAEKJOON27389.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BAEKJOON27389 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(a / 4);
+    }
+}
