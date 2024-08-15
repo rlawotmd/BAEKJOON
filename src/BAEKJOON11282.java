2b1f9538ae54b596918daae92cf946f427498d73
rlawotmd <kimjs2400@naver.com>
2024-08-15 23:25:02 +0900

    :memo: solved

---
+++ src/BAEKJOON11282.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON11282 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char b = (char) (a + 44031);
+        System.out.println(b);
+    }
+}
