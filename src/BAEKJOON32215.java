40dadd16190140abf1b01d40b7f3b483d88dc9c1
rlawotmd <kimjs2400@naver.com>
2024-10-02 19:37:00 +0900

    :memo: solved

---
+++ src/BAEKJOON32215.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BAEKJOON32215 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(m * k + m);
+    }
+}
