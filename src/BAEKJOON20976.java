07a404a10da0b85fc8fdc2a006a2d8e29f67d014
rlawotmd <kimjs2400@naver.com>
2024-10-03 23:19:43 +0900

    :memo: solved

---
+++ src/BAEKJOON20976.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BAEKJOON20976 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
