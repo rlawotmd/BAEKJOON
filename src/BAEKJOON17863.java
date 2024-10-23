5a32a9b7d79d80b91f5a8dd7e2003e7d1153d0ad
rlawotmd <kimjs2400@naver.com>
2024-10-23 19:31:44 +0900

    :memo: solved

---
+++ src/BAEKJOON17863.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BAEKJOON17863 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 5550000 && n <= 5559999) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
