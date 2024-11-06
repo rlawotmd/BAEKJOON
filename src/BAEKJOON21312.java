edb2e64ad219421c05d35ac3e46ff8d04c5b7786
rlawotmd <kimjs2400@naver.com>
2024-11-06 17:34:19 +0900

    :memo: solved

--- src/BAEKJOON21312.java
+++ src/BAEKJOON21312.java
@@ -5,12 +5,14 @@ public class BAEKJOON21312 {
         Scanner sc = new Scanner(System.in);
         int n = 1;
         int m = 1;
+        boolean f = false;
         for (int i = 0; i < 3; i++) {
             int num = sc.nextInt();
+            if (num == 1) f = true;
             if (num % 2 == 0) n *= num;
             else m *= num;
         }
-        if (m == 1) System.out.println(n);
+        if (m == 1 && !f) System.out.println(n);
         else System.out.println(m);
     }
 }
