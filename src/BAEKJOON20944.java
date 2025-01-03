e2b3116cd096b32062973512762193e4d1fa5705
김재승 <kimjs2400@naver.com>
2025-01-03 23:32:59 +0900

    :memo: solved

---
+++ src/BAEKJOON20944.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BAEKJOON20944 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        char [] arr = new char [num];
+
+        Arrays.fill(arr, 'a');
+
+        sb.append(arr);
+        System.out.println(sb.toString());
+    }
+}
