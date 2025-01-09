7e62dd6dcfd6e1766fe79638ac51c30018f217ad
김재승 <kimjs2400@naver.com>
2025-01-10 01:01:12 +0900

    :memo: solved

--- src/BAEKJOON25024.java
+++ src/BAEKJOON25024.java
@@ -9,35 +9,35 @@ public class BAEKJOON25024 {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            System.out.println("i: " + i);
             String[] input = br.readLine().split(" ");
             int num1 = Integer.parseInt(input[0]);
             int num2 = Integer.parseInt(input[1]);
             if (num1 < 24 && num2 < 60) {
                 sb.append("Yes ");
-            }
-            if (num1 >= 24 && num2 >= 60) {
+            } else {
                 sb.append("No ");
             }
 
             if (num1 == 1 || num1 == 3 || num1 == 5 || num1 == 7 || num1 == 8 || num1 == 10 || num1 == 12) {
                 if (num2 > 0 && num2 <= 31) {
-                    sb.append("Yes\n");
+                    sb.append("Yes" + "\n");
                 } else {
-                    sb.append("No\n");
+                    sb.append("No" + "\n");
                 }
             } else if (num1 == 2) {
                 if (num2 > 0 && num2 <= 29) {
-                    sb.append("Yes\n");
+                    sb.append("Yes" + "\n");
                 } else {
-                    sb.append("No\n");
+                    sb.append("No" + "\n");
                 }
             } else if (num1 == 4 || num1 == 6 || num1 == 9 || num1 == 11) {
                 if (num2 > 0 && num2 <= 30) {
-                    sb.append("Yes\n");
+                    sb.append("Yes" + "\n");
                 } else {
-                    sb.append("No\n");
+                    sb.append("No" + "\n");
                 }
+            } else {
+                sb.append("No" + "\n");
             }
         }
         System.out.println(sb);
