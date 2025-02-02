e8adac0f8330f556e3b5bed5902251ed476ebe7e
김재승 <kimjs2400@naver.com>
2025-02-02 23:57:40 +0900

    :memo: solved

---
+++ src/BAEKJOON32775.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BAEKJOON32775 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int f = sc.nextInt();
+
+        if (h > f) {
+            System.out.println("flight");
+        } else {
+            System.out.println("high speed rail");
+        }
+        sc.close();
+    }
+}
